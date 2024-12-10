360c0445436b7fa6668a372c59d0515df2e34330
ewaaaaannnn <ewanferg01@gmail.com>
2024-12-10 14:36:28 -0700

    init

---
+++ client/src/env.js
@@ -0,0 +1,9 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-2npqq1hq13hwvg56.us.auth0.com'
+export const clientId = 'LcnQKxiv9hmTERKQBe1b2rBIIJCCzcyS'
+export const audience = 'https://auth.speedwagonfoundation.com'
+
+
+
