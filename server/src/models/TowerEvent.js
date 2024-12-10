a569a0031c61f3eeea3cabe5c02bc8cc236f9580
ewaaaaannnn <ewanferg01@gmail.com>
2024-12-10 14:49:35 -0700

    Added Tower Scheme linked to DB

---
+++ server/src/models/TowerEvent.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+
+export const TowerEventSchema = new Schema({
+  creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+  name: { type: String, required: true, minlength: 3, maxLength: 50 },
+  description: { type: String, required: true, minlength: 15, maxlength: 1000 },
+  coverImg: { type: String, required: true },
+  location: { type: String, required: true, minLength: 1, maxLength: 500 },
+  capacity: { type: Number, required: true, min: 1, max: 5000 },
+  startDate: { type: Date, required: true },
+  isCanceled: { type: Boolean, required: true, default: false },
+  type: { type: String, enum: ['concert', 'convention', 'sport', 'digital'] }
+})
