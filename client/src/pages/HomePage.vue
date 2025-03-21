9a776bdd904fc52123bd07652a47e10153703bf1
ewaaaaannnn <ewanferg01@gmail.com>
2025-03-21 10:38:31 -0600

    Added Hero Image

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -28,21 +28,22 @@ async function getEvents() {
 </script>
 
 <template>
-  <div class="container my-5">
-    <div class="row text-center mb-5">
-      <h2 class="fw-bold">Welcome to Tower</h2>
-      <p class="text-muted mb-4 mx-auto" style="max-width: 800px;">
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin
-        literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-        College in Virginia, discovered the undoubtable source of Lorem Ipsum through a passage in classical literature.
-      </p>
-      <div class="d-flex justify-content-center gap-3">
-        <button v-if="account" data-bs-toggle="modal" data-bs-target="#eventModal"
-          class="btn btn-success text-light rounded-pill px-4">
-          Create Event
-        </button>
+  <div class="picture-bg">
+    <div class="container-fluid bg-hero d-flex flex-column justify-content-center">
+      <div class="row p-md-5 mt-md-5 text-bg kanit-regular text-md-start text-center">
+        <div class="col-12">
+          <h2 class="event-text ubuntu-regular text-light">Event management for people, by people</h2>
+        </div>
+        <div class="col-md-6 col-12">
+          <h4 class="event-text ubuntu-regular text-light">Whatever your interest, from hiking and reading to networking
+            and skill sharing, there
+            are thousands of people who share it on Tower. Events are happening every day- log in and join the fun.</h4>
+        </div>
       </div>
     </div>
+  </div>
+  <div class="container my-5">
+
 
     <section class="d-flex flex-wrap justify-content-center gap-2 gap-md-3 mb-4">
       <button @click="activeFilterCategory = 'all'" class="sort-button">All Events</button>
@@ -63,6 +64,20 @@ async function getEvents() {
 </template>
 
 <style scoped lang="scss">
+.bg-hero {
+  background-image: url('https://images.unsplash.com/photo-1517457373958-b7bdd4587205?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGZvcm1hbCUyMHBhcnR5fGVufDB8fDB8fHwy');
+  background-size: cover;
+  background-position: center;
+  height: 40em;
+  opacity: 75%;
+}
+
+
+.event-text {
+  text-shadow: 2px 2px 2px black;
+  color: #F8F6FF;
+}
+
 .container {
   max-width: 1200px;
   padding: 0 15px;
